e04b4e5941a224ecb2afc2225e6794cdf16d76a8
Alvin Quach <alvinquach91@gmail.com>
2022-09-11 19:51:13 -0700

    removed non-standard property

--- common-types/src/readonly/readonly-date.type.ts
+++ common-types/src/readonly/readonly-date.type.ts
@@ -17,7 +17,6 @@ export type ReadonlyDate = Pick<Date,
     'getUTCMinutes' |
     'getUTCMonth' |
     'getUTCSeconds' |
-    'getVarDate' |
     'toDateString' |
     'toISOString' |
     'toJSON' |
