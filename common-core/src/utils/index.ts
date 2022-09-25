be92235f4c831fc20163cfb94a1f444c8ca7df7b
Alvin Quach <alvinquach91@gmail.com>
2022-09-25 15:57:28 -0700

    fixed export name

--- common-core/src/utils/index.ts
+++ common-core/src/utils/index.ts
@@ -3,5 +3,5 @@ export * as CsvUtils from './csv.utils';
 export * as DateTimeUtils from './date-time.utils';
 export * as MapUtils from './map.utils';
 export * as MathUtils from './math.utils';
-export * as Object from './object.utils';
+export * as ObjectUtils from './object.utils';
 export * as SetUtils from './set.utils';
