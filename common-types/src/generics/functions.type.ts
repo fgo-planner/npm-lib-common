dca448b0be11ca1c5c73ad0dd11de6a61d393e79
Alvin Quach <alvinquach91@gmail.com>
2022-11-18 22:08:33 -0800

    added UnaryOperator

--- common-types/src/generics/functions.type.ts
+++ common-types/src/generics/functions.type.ts
@@ -1,5 +1,5 @@
 /**
- * Represents a supplier of results.
+ * Represents a function that accepts one argument and produces a result.
  */
 export type Function<T, R> = (arg1 : T) => R;
 
@@ -7,3 +7,11 @@ export type Function<T, R> = (arg1 : T) => R;
  * Represents a supplier of results.
  */
 export type Supplier<T> = () => T;
+
+/**
+ * Represents an operation on a single operand that produces a result of the
+ * same type as its operand. This is a specialization of Function for the case
+ * where the operand and result are of the same type.
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type UnaryOperator<T> = Function<T, T>;
